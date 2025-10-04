1a80befe28d410f03e013ca0e22a2c141c1c118e
neginikhil <neginikhil021@gmail.com>
2025-10-04 16:34:38 +0530

    Added Unit Testing

---
+++ src/test/java/org/main/TestClass.java
@@ -0,0 +1,57 @@
+package org.main;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestClass {
+
+    @Test
+    void testBruteForce(){
+        Main main = new Main();
+        int arr[] = {3, 5, -4, 8, 11, 1, -1, 6};
+        int res[] = main.bruteForce(arr,10);
+        Map<Integer, Integer> actualFreq = new HashMap<>();
+        for (int i : res) {
+            actualFreq.put(i, actualFreq.getOrDefault(i, 0) + 1);
+        }
+        Map<Integer,Integer> expectedFreq = new HashMap<>();
+        expectedFreq.put(11,1);
+        expectedFreq.put(-1,1);
+        assertEquals(actualFreq,expectedFreq);
+    }
+
+    @Test
+    void testTwoPointer(){
+        Main main = new Main();
+        int arr[] = {3, 5, -4, 8, 11, 1, -1, 6};
+        int res[] = main.twoPointer(arr,10);
+        Map<Integer, Integer> actualFreq = new HashMap<>();
+        for (int i : res) {
+            actualFreq.put(i, actualFreq.getOrDefault(i, 0) + 1);
+        }
+        Map<Integer,Integer> expectedFreq = new HashMap<>();
+        expectedFreq.put(11,1);
+        expectedFreq.put(-1,1);
+        assertEquals(actualFreq,expectedFreq);
+    }
+
+    @Test
+    void testUsingMap(){
+        Main main = new Main();
+        int arr[] = {3, 5, -4, 8, 11, 1, -1, 6};
+        int res[] = main.usingMap(arr,10);
+        Map<Integer, Integer> actualFreq = new HashMap<>();
+        for (int i : res) {
+            actualFreq.put(i, actualFreq.getOrDefault(i, 0) + 1);
+        }
+        Map<Integer,Integer> expectedFreq = new HashMap<>();
+        expectedFreq.put(11,1);
+        expectedFreq.put(-1,1);
+        assertEquals(actualFreq,expectedFreq);
+    }
+
+}
